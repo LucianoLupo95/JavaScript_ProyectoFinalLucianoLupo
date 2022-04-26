fc0e6855d24a8ece3d05919728db10323fcbde8b
Luciano Lupo <lnlupo95@gmail.com>
2022-04-25 23:15:04 -0300

    Proyecto Final Luciano Lupo

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 let agregar = document.querySelector("#agregar-receta");
 agregar.onclick = () =>{nuevaReceta()};
+let hoy = new Date();
+
+let yo = "Lucho";
+const agregarReceta = () =>{
+    Swal.fire({
+        title: '¡Felicitaciones!',
+        text: 'Agregaste una nueva receta',
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+    })
+    let fecha = document.querySelector("#fecha");
+    localStorage.setItem("Fecha", fecha.value);
+    let nombre = document.querySelector("#nombre");
+    localStorage.setItem("Nombre", nombre.value);
+    let foto = document.querySelector("#foto");
+    localStorage.setItem("Foto", foto.value);
+    let dificultad = document.querySelector("#dificultad");
+    localStorage.setItem("Dificultad", dificultad.value);
+    let ingredientes = document.querySelector("#ingredientes");
+    localStorage.setItem("Ingredientes", ingredientes.value);
+    let preparacion = document.querySelector("#preparacion");
+    localStorage.setItem("Preparacion", preparacion.value);
+    return false;
+}
 
 const nuevaReceta = () =>{
     let parrafo = document.createElement("form");
     parrafo.innerHTML = `
-    <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-    </div>
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+        <form class="p-5 formulario-receta" >
+            <label for="nombre" class="form-label">Fecha</label>
+            <input type="date" id="fecha" name="fecha" class="form-control">
+            <label for="nombre" class="form-label">Nombre</label>
+            <input type="text" id="nombre" name="nombre" class="form-control">
+            <label for="foto" class="form-label">Foto</label>
+            <input type="file" id="foto" name="foto" class="form-control">
+            <label for="dificultad" class="form-label">Dificultad</label>
+            <input type="text" id="dificultad" name="dificultad" class="form-control">
+            <label for="ingredientes" class="form-label">Ingredientes</label>
+            <input type="text" id="ingredientes" name="ingredientes" class="form-control">
+            <label for="preparacion" class="form-label">Preparacion</label>
+            <input type="text" id="preparacion" name="preparacion" class="form-control">
+            <button onclick="agregarReceta(); return false" type="submit" class="btn btn-primary">Agregar</button>
+        </form>
     `; 
-    document.body.append(parrafo);
+    document.body.appendChild(parrafo);
+    agregar.remove();
 }
+
